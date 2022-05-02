5aaee3b54d6490f99bcf6f21b438f0e0eea23d66
mindlove4 <60215505+mindlove4@users.noreply.github.com>
2022-05-02 12:53:29 +0700

    Update settings.py

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ltn(1d*-ekpd57$-ba=wz#t6nivglrklgc%08gz2u^nuy6a*&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
