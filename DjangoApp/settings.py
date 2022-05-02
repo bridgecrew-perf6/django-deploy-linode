e2fcaeeff5b9f53a385683be1d98fbf422dd674e
mindlove4 <60215505+mindlove4@users.noreply.github.com>
2022-05-02 13:23:16 +0700

    Update settings.py

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cisweb',
+    'allauth',
     'django.contrib.sites',,
     'allauth.account',
     'allauth.socialaccount',
